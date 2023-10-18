cabba4638e47df64c978bc4824fcbc4b35847d0e
David Delassus <david.jose.delassus@gmail.com>
2023-10-19 00:33:35 +0200

    :sparkles: add quote command

--- lib/idlebot/commands.ex
+++ lib/idlebot/commands.ex
@@ -20,6 +20,32 @@ defmodule IdleBot.Commands do
     end
     {:ok, continuation}
   end
+  def dispatch(["quote"]) do
+    continuation = fn sender, channel, %State{} = state ->
+      msg = case IdleBot.QuoteDB.get_random(channel) do
+        :none -> "No quotes for this channel"
+        {:some, {author, text}} -> "> #{text} -- #{author}"
+      end
+      ExIRC.Client.msg(state.client, :privmsg, channel, msg)
+      :ok
+    end
+    {:ok, continuation}
+  end
+  def dispatch(["quote", "add", author | text]) when length(text) > 0 do
+    text = text |> Enum.join(" ")
+    continuation = fn sender, channel, %State{} = state ->
+      IdleBot.QuoteDB.add(channel, author, text)
+      ExIRC.Client.msg(state.client, :privmsg, channel, "#{sender.nick}: Thank you for your contribution")
+      :ok
+    end
+    {:ok, continuation}
+  end
+  def dispatch(["quote" | _]) do
+    {:error, {
+      :invalid_command,
+      {:quote, :usage, ["!quote", "quote add <author> <text>"]}
+    }}
+  end
   def dispatch(cmd) do
     {:error, :unknown_command}
   end
